33602c3736287ab0c894371723425183c4d9dd4c
fabLAB Asturias <fablab@laboralcentrodearte.org>
2016-04-23 18:08:36 +0000

    ds18b20 exception

--- ds18b20.py
+++ ds18b20.py
@@ -4,14 +4,17 @@ class Ds18b20:
 	def __init__(self, address):
 		self.sysfile="/sys/bus/w1/devices/"+address+"/w1_slave"
 	def read(self):
-		f = open(self.sysfile)
-		text = f.read()
-		f.close()
+		try:
+			f = open(self.sysfile)
+			text = f.read()
+			f.close()
 		
-		data = text.split("\n")[1].split(" ")[9]
-		temp = float(data[2:])
-		temp = temp/1000
+			data = text.split("\n")[1].split(" ")[9]
+			temp = float(data[2:])
+			temp = temp/1000
 
-		return temp
+			return temp
+		except: 
+			return 0
 
 
